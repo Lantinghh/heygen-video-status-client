256aebe9c752ec7a607a98c5e2c8e9266e5a8e20
Lantinghh <hlt1298@163.com>
2024-12-23 04:45:10 -0800

    add test integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,12 +1,16 @@
 import subprocess
 import time
+import sys
+import os
 
-from client_ import VideoStatusClient
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from client.client_ import VideoStatusClient
 
 def test_integration():
-    # 启动服务器
+    # start sercver
     server_process = subprocess.Popen(['python', 'server/app.py'])
-    time.sleep(2)  # 等待服务器启动
+    time.sleep(2)  # waiting for server to start
 
     try:
         client = VideoStatusClient("http://127.0.0.1:5000", max_retries=10)
